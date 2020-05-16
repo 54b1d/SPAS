4bb87121cb3102c5fe91d4f992d491cdaced08cb
54b1d <sabidul1@gmail.com>
2020-05-16 13:23:46 +0600

    moved functions from main to functions.py adjusted imports in main removed lambda when calling functions

--- functions.py
+++ functions.py
@@ -1,8 +1,9 @@
 import datetime
+import sqlite3
 
-from PyQt5 import uic
+from PyQt5 import QtWidgets, uic
 
-from SqliteHelper import code
+from SqliteHelper import code, transactions
 
 
 def add_transaction_in_ui():
@@ -294,3 +295,91 @@ def initial_adjustment():
 
     ui.show()
     ui.accepted.connect(confirmed)
+
+
+def ledger_transactions(uid=''):
+    ledger = uic.loadUi('ledger.ui')
+    # load Journal UI
+    """Ledger Widget table contents"""
+    data = transactions(uid)
+    for row_number, row_data in enumerate(data):
+        ledger.tableWidget.insertRow(row_number)
+        for column_number, column_data in enumerate(row_data):
+            cell = QtWidgets.QTableWidgetItem(str(column_data))
+            ledger.tableWidget.setItem(row_number, column_number, cell)
+
+
+def journal():
+    journalWindow = uic.loadUi('viewJournal.ui')
+    """calls sqlitehelper select"""
+    journalWindow.show()
+    # set as requested date and day name for that
+    journalWindow.label_date_day.setText(
+        str(datetime.date.today()) + '- ' + str(datetime.datetime.now(
+        ).strftime("%A")))
+    requested_date = str(datetime.date.today)  # TODO assign date picker
+    query = '''SELECT * FROM [transactions] WHERE [tdate] = ?;'''
+    param = (requested_date,)
+    try:
+        data = code.select(query, param)
+        print(data)  # debug
+    except sqlite3.Error as e:
+        print(e)
+
+    # load Add Trx in function
+    journalWindow.pb_newTrx_in.clicked.connect(add_transaction_in_ui)
+    # load Add Trx out function
+    journalWindow.pb_newTrx_out.clicked.connect(add_transaction_out_ui)
+
+
+def accounts():
+    """loads accounts window"""
+    # load Accountslist UI
+    accountsWindow = uic.loadUi('viewAccounts.ui')
+    # UI Add New Account Dialogue
+    addacc = uic.loadUi('addAccountDialogue.ui')
+    accountsWindow.show()
+
+    def fetch_accounts():
+        """populate accounts window table"""
+        accountsWindow.tableWidget.setRowCount(0)
+        query = '''SELECT * FROM accounts'''
+        try:
+            data = code.select(query)
+            # accountsWindow.tableWidget.setColumnCount(2)
+            for row_number, row_data in enumerate(data):
+                accountsWindow.tableWidget.insertRow(row_number)
+                for column_number, info in enumerate(row_data):
+                    celldata = QtWidgets.QTableWidgetItem(str(info))
+                    accountsWindow.tableWidget.setItem(
+                        row_number, column_number, celldata)
+        except sqlite3.Error as e:
+            print('Error', e)
+        accountsWindow.tableWidget.setEditTriggers(
+            QtWidgets.QTreeView.NoEditTriggers)
+
+    fetch_accounts()
+    accountsWindow.pbViewAccountsRefresh.clicked.connect(fetch_accounts)
+
+    def pbaddaccount_clicked():
+        """check/grab data and passes to sqliteHelper.insert_acc"""
+        name = str(addacc.ldname.text())
+        address = str(addacc.ldaddress.text())
+        mobile = str(addacc.ldmobile.text())
+        if name != '' and address != '':  # eliminates empty data
+            data = (name, address, mobile)
+            code.insert_account(data)
+        else:
+            print('Required fields are empty.')
+            addacc.show()
+        # after insert reset fields
+        addacc.ldname.setText('')
+        addacc.ldaddress.setText('')
+        addacc.ldmobile.setText('')
+        addacc.show()
+        addacc.ldname.setFocus()
+
+    # accountsWindow.pbViewAccountsRefresh.clicked.connect()
+    # calls function to refresh account list
+    accountsWindow.pbAddAccount.clicked.connect(addacc.show)  # opens addAcc ui
+    addacc.buttonBox.accepted.connect(pbaddaccount_clicked)

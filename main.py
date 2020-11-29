0ebeb81165efd91bf37e062bfe452e93e47f1751
54b1d <sabidul1@gmail.com>
2020-11-29 14:42:55 +0600

    Removed unnecessary comments

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# initial concept musicamante@stackoverflow
-# With some modification from my side @54b1d
-
 from functools import partial
 from PyQt5 import uic
 from PyQt5.QtWidgets import *
@@ -8,10 +5,9 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime,
                           QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
 from PyQt5.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase,
-                         QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
+                         QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap,
+                         QRadialGradient)
 
-# from pages.first import First
-# from pages.second import Second
 from ui_functions import *
 
 
@@ -23,14 +19,6 @@ class MainWindow(QMainWindow):
         uic.loadUi('ui/mainWindow.ui', self)
         self.Btn_Toggle.clicked.connect(
             lambda: UIFunctions.toggleMenu(self, 250, True))
-
-        # load imported page classes
-        # self.first = First()
-        # self.second = Second()
-
-        # add pages to stack
-        # self.stackedWidget.addWidget(self.first)
-        # self.stackedWidget.addWidget(self.second)
 
         # list buttons available in main ui
         buttons = (self.pb_home, self.pb_cash_flow, self.pb_buy_sale)

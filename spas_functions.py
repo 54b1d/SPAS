a8cb85c74ee81a354f655f50e0de74cc5ecea1a9
54b1d <sabidul1@gmail.com>
2020-12-05 22:03:45 +0600

    accounts functions added
    Fetch_accounts replacing old accounts function
    
    init_add_accounts

--- spas_functions.py
+++ spas_functions.py
@@ -63,96 +63,27 @@ def ledger_transactions(uid=''):
             ledger.tableWidget.setItem(row_number, column_number, cell)
 
 
-def accounts():
-    """loads accounts window"""
-    accounts_window = uic.loadUi('viewAccounts.ui')
-
-    # UI Add New Account Dialogue
-    add_acc_ui = uic.loadUi('addAccountDialogue.ui')
-
-    def fetch_accounts():
-        """populate accounts window table"""
-        accounts_window.tableWidget.setRowCount(0)
-        query = '''SELECT
-       [accounts].[name],
-       [accounts].[address],
-       [accounts].[mobile],
-       [balance].[dr_amount],
-       [balance].[cr_amount] FROM
-       [accounts]
-       INNER JOIN [balance] ON [accounts].[uid] = [balance].[uid]
-       ORDER BY [accounts].[name];'''
-
-        try:
-            data = database.select(query)
-            # accounts_window.tableWidget.setColumnCount(2)
-            for row_number, row_data in enumerate(data):
-                accounts_window.tableWidget.insertRow(row_number)
-                for column_number, info in enumerate(row_data):
-                    celldata = QtWidgets.QTableWidgetItem(str(info))
-                    accounts_window.tableWidget.setItem(
-                        row_number, column_number, celldata)
-        except sqlite3.Error as err:
-            print('Error', err)
-        accounts_window.tableWidget.setEditTriggers(
-            QtWidgets.QTreeView.NoEditTriggers)
-
-    fetch_accounts()
-    accounts_window.pbViewAccountsRefresh.clicked.connect(fetch_accounts)
-
-    def pb_add_account_clicked():
-        """check/grab data and passes to sqliteHelper.insert_acc"""
-        name = str(add_acc_ui.ldname.text())
-        address = str(add_acc_ui.ldaddress.text())
-        mobile = str(add_acc_ui.ldmobile.text())
-        if name != '' and address != '':  # eliminates empty data
-            data = (name, address, mobile)
-            database.insert_account(data)
-        else:
-            print('Required fields are empty.')
-            add_acc_ui.show()
-        # after insert reset fields
-        add_acc_ui.ldname.setText('')
-        add_acc_ui.ldaddress.setText('')
-        add_acc_ui.ldmobile.setText('')
-        add_acc_ui.show()
-        add_acc_ui.ldname.setFocus()
-
-    # accounts_window.pbViewAccountsRefresh.clicked.connect()
-    # calls function to refresh account list
-    accounts_window.pbAddAccount.clicked.connect(
-        add_acc_ui.show)  # opens addAcc ui
-    add_acc_ui.buttonBox.accepted.connect(pb_add_account_clicked)
-    accounts_window.show()
-
-
-def init_ledger(uid=None):
-    global ledgerUi
-    ledgerUi = uic.loadUi("ledger.ui")
-    query = '''SELECT
-       [transactions].[tdate],
-       [transactions].[c_in],
-       [transactions].[c_out],
-       [transactions].[balance_payable],
-       [transactions].[balance_receivable]
-        FROM   [transactions] WHERE [transactions].[uid] = ?
-        ORDER BY [transactions].[tdate];'''
-    param = (uid,)
-    """data = transactions(uid)
-    for row_number, row_data in enumerate(data):
-        ledger.tableWidget.insertRow(row_number)
-        for column_number, column_data in enumerate(row_data):
-            cell = QtWidgets.QTableWidgetItem(str(column_data))
-            ledger.tableWidget.setItem(row_number, column_number, cell)"""
-    data = database.select(query, param)
-    for row_number, row_data in enumerate(data):
-        t_date, c_in, c_out, balance_payable, balance_receivable = row_data
-        ledgerUi.tableWidget.insertRow(row_number)
-        for column_number, column_data in enumerate(row_data):
-            cell = QtWidgets.QTableWidgetItem(str(column_data))
-            ledgerUi.tableWidget.setItem(row_number, column_number, cell)
-        print(str(row_data))
-    ledgerUi.show()
+def fetch_accounts(table):
+    """populate accounts window table"""
+    table.setRowCount(0)
+    query = '''SELECT 
+   [accounts].[name], 
+   [accounts].[address], 
+   [accounts].[mobile]
+    FROM   [accounts];'''
+    try:
+        data = database.select(query)
+        # accounts_window.tableWidget.setColumnCount(2)
+        for row_number, row_data in enumerate(data):
+            table.insertRow(row_number)
+            for column_number, info in enumerate(row_data):
+                celldata = QtWidgets.QTableWidgetItem(str(info))
+                table.setItem(
+                    row_number, column_number, celldata)
+    except sqlite3.Error as err:
+        print('Error', err)
+    table.setEditTriggers(
+        QtWidgets.QTreeView.NoEditTriggers)
 
 
 # get uid for specific Name supplied
@@ -276,3 +207,37 @@ def cash_flow_out_table(self):
         for column_number, column_data in enumerate(row_data):
             cell = QtWidgets.QTableWidgetItem(str(column_data))
             self.setItem(row_number, column_number, cell)
+
+
+def init_add_account():
+    add_account_ui = uic.loadUi('ui/diagNewAccount.ui')
+    add_account_ui.show()
+    # after insert reset fields
+    add_account_ui.ld_name.setText('')
+    add_account_ui.ld_address.setText('')
+    add_account_ui.ld_mobile.setText('')
+    add_account_ui.ld_name.setFocus()
+
+    # todo get items from settings configuration
+    add_account_ui.comboGroup.addItem("Payable/Receivable", "PR")
+
+    def confirmed():
+        name = add_account_ui.ld_name.text()
+        address = add_account_ui.ld_address.text()
+        mobile = add_account_ui.ld_mobile.text()
+        group = add_account_ui.comboGroup.currentData()
+        if name != '' and address != '':  # eliminates empty data
+            data = (name, address, mobile, group)
+            try:
+                database.insert_account(data)
+                msg = name + "- Added successfully."
+                add_account_ui.label_msg.setText(msg)
+            except sqlite3.Error as err:
+                add_account_ui.label_msg.setText(err)
+        else:
+            msg = 'Required fields are empty.'
+            add_account_ui.show()
+            add_account_ui.label_msg.setText(msg)
+
+    add_account_ui.pb_confirm.clicked.connect(confirmed)
+    add_account_ui.pb_cancel.clicked.connect(lambda: add_account_ui.close())

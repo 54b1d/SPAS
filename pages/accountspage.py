ff6e323a65787a697fbbc6a128515d46da348765
54b1d <sabidul1@gmail.com>
2020-12-05 22:08:33 +0600

    actions updated
    populate accounts table
    
    pb_add_account action added

--- pages/accountspage.py
+++ pages/accountspage.py
@@ -1,6 +1,8 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import QWidget
 
+from spas_functions import init_add_account, fetch_accounts
+
 
 class AccountsPage(QWidget):
     # Class name is important as its imported
@@ -9,3 +11,5 @@ class AccountsPage(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         uic.loadUi('ui/accounts.ui', self)
+        fetch_accounts(self.table_accounts)
+        self.pb_add_account.clicked.connect(init_add_account)

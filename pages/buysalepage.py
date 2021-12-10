f8ad223df7b9e0ca32fb9f6c252ec8fe34928bd5
54b1d <54b1d@users.noreply.github.com>
2021-12-11 00:29:27 +0600

    buysalepage.py: Fixed Inventory Table not loading data

--- pages/buysalepage.py
+++ pages/buysalepage.py
@@ -1,7 +1,7 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import QWidget
 
-from spas_functions import init_inv_transaction, init_add_product
+from spas_functions import init_inv_transaction, fetch_inventory, init_add_product
 
 
 class BuySale(QWidget):
@@ -11,6 +11,7 @@ class BuySale(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         uic.loadUi('ui/buySale.ui', self)
+        fetch_inventory(self.table_inventory)
         self.pb_buy.clicked.connect(lambda: init_inv_transaction("BUY"))
         self.pb_sale.clicked.connect(lambda: init_inv_transaction("SALE"))
         self.pb_add_new_product.clicked.connect(lambda: init_add_product())

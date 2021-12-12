9cef692d09dda449c44c3757959c059fee40c455
54b1d <54b1d@users.noreply.github.com>
2021-12-13 00:49:09 +0600

    Fixed Startup Error Load Setup UI if DB is not found

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from setup import prerequisites
+from setup import prerequisites, list_databases
 from functools import partial
 from PyQt5 import uic
 from PyQt5.QtWidgets import *
@@ -11,6 +11,7 @@ from PyQt5.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFont
 
 from ui_functions import *
 from spas_functions import first_run
+from configparser import ConfigParser
 
 
 class MainWindow(QMainWindow):
@@ -33,10 +34,78 @@ class MainWindow(QMainWindow):
                 partial(self.stackedWidget.setCurrentIndex, i))
 
 
+class SetupWindow(QDialog):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # load Main Window UI here
+        uic.loadUi('ui/setup.ui', self)
+        self.rb_use_existing_database.clicked.connect(lambda: use_existing())
+        config_file = 'Settings.ini'
+        config_obj = ConfigParser()
+        config_obj.read(config_file)
+
+        def use_existing():
+            if self.rb_use_existing_database.isChecked():
+                print("Checked")
+                self.ld_dbname.setEnabled(False)
+            else:
+                print("Not checked")
+                self.ld_dbname.setEnabled(True)
+
+        databases = list_databases()
+        for out in databases:
+            self.combo_databases.addItem(out)
+
+        print("Setting up application")
+
+        self.accepted.connect(lambda: check_values(config_file))
+
+        def check_values(config_file):
+            business_name = str(self.ld_business_name.text())
+            business_address = str(self.ld_business_address.text())
+            if business_name != '' and business_address != '':
+                store_settings_confirmed(config_file)
+            else:
+                msg = 'Insert required fields...'
+                print(msg)
+                self.label_msg.setText(msg)
+
+        def store_settings_confirmed(config_file):
+            business_name = str(self.ld_business_name.text())
+            business_address = str(self.ld_business_address.text())
+            if self.rb_use_existing_database.isChecked():
+                dbname = str(self.combo_databases.currentText())
+                print('existing', dbname)
+            else:
+                dbname = str(self.ld_dbname.text()) + '.db'
+
+            # store in config_file
+            print(business_name, business_address, dbname)
+            config_obj["USERINFO"] = {}
+            user_info = config_obj["USERINFO"]
+            user_info["business_name"] = business_name
+            user_info["business_address"] = business_address
+
+            config_obj["APPSETTINGS"] = {}
+            app_settings = config_obj["APPSETTINGS"]
+            # set as not first run
+            app_settings["first_run"] = "No"
+            app_settings["dbname"] = dbname
+            with open(config_file, 'w') as settings:
+                config_obj.write(settings)
+            print("Settings Stored")
+
+
 if __name__ == '__main__':
     app = QApplication([])
-    window = MainWindow()
+    # if prerequisites returns dbname then continue
+    # else run setup ui
     if prerequisites():
+        window = MainWindow()
         first_run()
-        window.show()
+    else:
+        print("loading setup ui...")
+        window = SetupWindow()
+    window.show()
     app.exec_()

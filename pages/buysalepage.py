791e5633375b015d97307f34d93cbf2f0dc52ad2
54b1d <54b1d@users.noreply.github.com>
2021-12-17 21:38:21 +0600

    buysalepage.py: Refreshing Tables simplified
    Rather than calling table refresh individually, call refresh function once. All individual table refresh code will be inside refresh_page function

--- pages/buysalepage.py
+++ pages/buysalepage.py
@@ -1,7 +1,7 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import QWidget
 
-from spas_functions import init_inv_transaction, fetch_inventory, init_add_product
+from spas_functions import init_inv_transaction, fetch_inventory, fetch_inventory_transactions, init_add_product
 
 
 class BuySale(QWidget):
@@ -12,7 +12,13 @@ class BuySale(QWidget):
         super().__init__(*args, **kwargs)
         uic.loadUi('ui/buySale.ui', self)
         fetch_inventory(self.table_inventory)
+
+        def refresh_page(self):
+            fetch_inventory_transactions('BUY', self.table_inventory_buy)
+            fetch_inventory_transactions('SALE', self.table_inventory_sale)
+
+        refresh_page(self)
         self.pb_buy.clicked.connect(lambda: init_inv_transaction("BUY"))
         self.pb_sale.clicked.connect(lambda: init_inv_transaction("SALE"))
         self.pb_add_new_product.clicked.connect(lambda: init_add_product())
-        self.pb_refresh_inventory.clicked.connect(lambda: fetch_inventory(self.table_inventory))
+        self.pb_refresh_inventory.clicked.connect(lambda: refresh_page(self))
